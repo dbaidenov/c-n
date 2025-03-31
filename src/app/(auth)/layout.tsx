a09ca67f6d302e239df002bdfa781ab3f7919df2
Damir <damir@myterior.kz>
2025-03-31 19:32:38 +0500

    start auth

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from "react";
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-background">
+      {children}
+    </div>
+  );
+}
